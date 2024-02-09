720279662c1594f0b9b03484d93c8606ac75bd77
imanthaEpic <imantha_o@epiclanka.net>
2024-02-09 12:01:38 +0530

    Styled with Tailwind

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -32,7 +32,7 @@ public class LocationController {
     @RequestMapping("/saveLoc")
     public String saveLocation(@ModelAttribute("location") Location location, RedirectAttributes redirectAttributes) {
         Location locationSaved = service.saveLocation(location);
-        String msg = "Location saved with id: " + locationSaved.getId();
+        String msg = "Location saved successfully!";
 
         // Add the message as a flash attribute
         redirectAttributes.addFlashAttribute("msg", msg);
